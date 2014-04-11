5c4d25694700e89fe49d830bfb591a36651386d9
Nicolas Bastien <nbastien@prestaconcept.net>
2014-04-11 15:07:17 +0200

    Initialize bundle

---
+++ PrestaPimpMySonataBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the PrestaPimpMySonataBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\PimpMySonataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaPimpMySonataBundle extends Bundle
+{
+}
